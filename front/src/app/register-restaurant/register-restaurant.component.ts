a438275ded8188b4e754f0233ac9bb9864dbdb47
AdrienForestier <adrien.frstr@gmail.com>
2022-11-09 17:48:24 +0100

    feat : restaurant registration2

---
+++ front/src/app/register-restaurant/register-restaurant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-restaurant',
+  templateUrl: './register-restaurant.component.html',
+  styleUrls: ['./register-restaurant.component.scss']
+})
+export class RegisterRestaurantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
